4dc589d7f6275aa5b06063dd0d7eea2dfe8ad6d0
Jared Forth <jared.forth@rivalmind.com>
2019-03-28 17:00:32 -0500

    added virtual DOM

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 const assert = require('assert');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const dom = new JSDOM(``);
 
 // Write sample unit test
 
@@ -7,5 +11,12 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+  });
+});
+
+
+describe('setValue', function() {
+  it('should return the inner text of a passed element', function() {
+    assert.equal(setValue(data, val), 10);
   });
 });
